fffa26f1d571e69871d5fa9a83b7a37e3b54e58c
ABHI-SHEK-SINGH <abhishekbtp05@gmail.com>
2022-07-09 19:23:48 +0530

    sign in html file

--- js/style.js
+++ js/style.js
@@ -38,3 +38,22 @@ var swiper = new Swiper(".popular-content", {
         },
     },
   });
+  // show video
+  let playButton = document.querySelector('.play-movie');
+  let video = document.querySelector('.video-container');
+  let myvideo = document.querySelector('#myvideo');
+  let closebtn = document.querySelector('.close-video');
+
+  playButton.onclick = () =>{
+    video.classList.add('show-video');
+    // auto play when click on btn
+    myvideo.play();
+
+  }
+  closebtn.onclick = () =>{
+    video.classList.remove('show-video');
+    // pause on click on btn
+    myvideo.pause();
+
+  }
+  
